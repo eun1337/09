7a3f4c2ce6f4c284802d985cfabd788490f3a416
eun1337 <dbdmstj0327@naver.com>
2023-11-03 18:29:52 +0900

    02

--- main.c
+++ main.c
@@ -6,14 +6,17 @@
 int main(int argc, char *argv[]) 
 {
 	int i;
+	int average, sum = 0;
 	int grade[SIZE];
 	printf("5명의 점수를 입력하세요.\n");
 	
-	for(i = 0; i < SIZE; i++)
+	for(i = 0; i < SIZE; i++) {
 		scanf("%d", &grade[i]);
+		sum+= grade[i];
+	}
 	
-	for(i = 0; i < SIZE; i++) 
-		printf("grade[%d] = %d\n", i, grade[i]);
+	average = sum / SIZE;
+	printf("score average : %d\n", average); 
 	
 	return 0;
 }
